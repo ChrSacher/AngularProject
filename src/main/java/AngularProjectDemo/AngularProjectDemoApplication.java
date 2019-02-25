b92a263ba116cd87a8f1e278a5d4ca9a05e9e236
Christian Sacher <reyfin@gmx.de>
2019-02-25 18:51:28 +0100

    Working Project with Angular

---
+++ src/main/java/AngularProjectDemo/AngularProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package AngularProjectDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularProjectDemoApplication.class, args);
+	}
+
+}
