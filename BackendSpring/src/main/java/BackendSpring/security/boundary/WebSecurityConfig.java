866a6f6746b534d5f72b61f09f5380e683b50d16
Christian Sacher <reyfin@gmx.de>
2019-03-03 20:51:59 +0100

    Introduce Jwt and Oauth

---
+++ BackendSpring/src/main/java/BackendSpring/security/boundary/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package BackendSpring.security.boundary;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import BackendSpring.security.service.user.CurrentUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource(name = "userService")
+    private CurrentUserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+	web.ignoring().antMatchers("/css/**", "/h2-console/**", "/console/**", "/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+	http.csrf().disable()
+            .anonymous().disable()
+            .authorizeRequests()
+            .antMatchers("**").permitAll();
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+	return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+	return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+	auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    }
+}
